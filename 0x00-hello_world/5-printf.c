75e702176f40eb05bff53a8265c17618a68df457
Joel <sedemboafo@gmail.com>
2023-03-19 09:19:34 -0700

    ad

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - now using printf not put
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
