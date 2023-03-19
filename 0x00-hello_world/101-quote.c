1d59ee7f78c8940bd6f5b907c374ec155fd0b703
Joel <sedemboafo@gmail.com>
2023-03-19 10:26:42 -0700

    jkd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - last one
+ *
+ * Return: 1
+ *
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
