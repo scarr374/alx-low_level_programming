f437ed3c4e975459a56567346f6ec18fd7cd068f
Joel <sedemboafo@gmail.com>
2023-03-19 09:10:47 -0700

    the

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+ * main - Putc function
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
