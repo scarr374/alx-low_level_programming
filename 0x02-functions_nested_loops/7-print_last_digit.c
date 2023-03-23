75aca0bebcd1a3e8262edaa3d14bfec4d29c6bdd
Joel <sedemboafo@gmail.com>
2023-03-22 21:45:06 -0700

    pjds

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last
+ * @n: The number to be treated
+ * Return: valude
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
